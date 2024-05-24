b4236912accbf905371a05e59d4c666d97468f44
GiampyLux <giampaolopaladino01@gmail.com>
2024-05-24 12:38:37 +0200

    creata error 404 e fatto routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,43 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AudiComponent } from './pages/audi/audi.component';
+import { FordComponent } from './pages/ford/ford.component';
+import { FiatComponent } from './pages/fiat/fiat.component';
+import { Error404Component } from './pages/error404/error404.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{
+  path:"",
+  redirectTo: "/home",
+  pathMatch: "full"
+},
+{
+  path:"home",
+  component: HomeComponent,
+  title: "home"
+},
+{
+  path:"audi",
+  component: AudiComponent,
+  title: "audi"
+},
+{
+  path:"ford",
+  component: FordComponent,
+  title: "ford"
+},
+{
+  path:"fiat",
+  component: FiatComponent,
+  title: "fiat"
+},
+{
+  path:"**",
+  component: Error404Component,
+  title: "error 404"
+},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,22 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './main-components/navbar/navbar.component';
 import { FooterComponent } from './main-components/footer/footer.component';
+import { FiatComponent } from './pages/fiat/fiat.component';
+import { FordComponent } from './pages/ford/ford.component';
+import { AudiComponent } from './pages/audi/audi.component';
+import { HomeComponent } from './pages/home/home.component';
+import { Error404Component } from './pages/error404/error404.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    FooterComponent
+    FooterComponent,
+    FiatComponent,
+    FordComponent,
+    AudiComponent,
+    HomeComponent,
+    Error404Component,
   ],
   imports: [
     BrowserModule,
