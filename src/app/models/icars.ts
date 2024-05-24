80a1af6ce570ffeaacfd85ee18f8980c7cee8bb1
GiampyLux <giampaolopaladino01@gmail.com>
2024-05-24 15:12:51 +0200

    aggiornamenti wow

---
+++ src/app/models/icars.ts
@@ -0,0 +1,11 @@
+export type Root = Icars[]
+
+export interface Icars {
+  brand: string
+  brandLogo: string
+  model: string
+  modelImage: string
+  year: number
+  price: number
+  available: boolean
+}
